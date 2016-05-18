c8184af77adcd97ce1a99d1dfebb089f21a42545
sotheareth <ham8086sotheareth@yahoo.com>
2016-05-18 15:25:23 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,11 +8,6 @@ if ('serviceWorker' in navigator) {
     console.log('Service Worker is ready :^)', reg);
     reg.pushManager.subscribe({userVisibleOnly: true}).then(function(sub) {
 	document.getElementById("result").innerHTML = sub.endpoint;
-	var endpoint = document.getElementById("result").innerHTML;
-	var subscriptionId = endpoint.substring(endpoint.lastIndexOf("/")+1);
-	document.getElementById("subscriptionId").innerHTML = subscriptionId;
-	var channelName = document.getElementById("subject").value;
-	window.open("https://192.168.1.143:8443/AppServerPushSpring/user/register?subscriptionId=" + subscriptionId + "&channelName=" + channelName);
     });
   }).catch(function(error) {
     console.log('Service Worker error :^(', error);
