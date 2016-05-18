8610f809b6d76ebb3cce2d23b6e361ea3993f362
sotheareth <ham8086sotheareth@yahoo.com>
2016-05-18 10:38:28 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,8 +7,10 @@ if ('serviceWorker' in navigator) {
   }).then(function(reg) {
     console.log('Service Worker is ready :^)', reg);
     reg.pushManager.subscribe({userVisibleOnly: true}).then(function(sub) {
-		document.getElementById("result").innerHTML = sub.endpoint;
-      console.log('endpoint:', sub.endpoint);
+	document.getElementById("result").innerHTML = sub.endpoint;
+	var endpoint = document.getElementById("result").innerHTML;
+	var subscriptionId = endpoint.substring(endpoint.lastIndexOf("/")+1);
+	document.getElementById("result").innerHTML = subscriptionId;
     });
   }).catch(function(error) {
     console.log('Service Worker error :^(', error);
